4f7d7c735be0c7856be43e9450a8baa74abd3e34
vagrant <vagrant@homestead>
2018-05-30 05:36:14 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts/default')
 @section('content')
-  <h1>this is the sample home page</h1>
+    <div class="jumbotron">
+        <h1>Person of Interest / POI</h1>
+        <p class="lead">
+           <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">
+            PERSON OF INTEREST </a>is a crime thriller about an ex-CIA agent and a mysterious billionaire who prevent violent crimes by utilizing an all-seeing machine that can predict the events before they happen. Although John Reese and Harold Finch have successfully prevented numerous crimes thanks to The Machine’s omniscience, the team was unable to stop the malevolent artificial
+        </p>
+        <p>
+            Coming soon ...
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">reigister now</a>
+        </p>
+    </div>
 @stop
