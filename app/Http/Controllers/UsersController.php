5517ea0adf6bbda8c746e7dfc43da19c40f6751b
lhz <sinian1860@126.com>
2018-06-08 18:07:42 +0800

    八章内容

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,7 +20,12 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-    public function store(Request $request)
+    //修改
+    public function edit(User $user){
+        return view('users/edit', compact('user'));
+    }
+    //保存添加
+    public function store(User $user, Request $request)
     {
         $this->validate($request, [
             'name'  =>  'required|max:50',
@@ -33,7 +38,21 @@ class UsersController extends Controller
             'email'     =>  $request->email,
             'password'  =>  bcrypt($request->password)
         ]);
+        Auth::login($user);
         session()->flash('success','welcome to the  new world!');
         return redirect()->route('users.show',[$user]);
+    }
+    //保存修改
+    public function update(Request $request){
+        $this->validate($request, [
+            'name'  => 'required|max:50',
+            'password'  =>  'required|confirmed|min:6'
+        ]);
+
+        $user->update([
+            'name'  =>  $request->name,
+            'password'  =>  bcrypt($request->password)
+        ]);
+
     }
 }
